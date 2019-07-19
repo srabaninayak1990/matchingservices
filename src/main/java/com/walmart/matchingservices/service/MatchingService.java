982796662cf8ded4dbb40c57bd9958bfab5cd969
Srabani Nayak <srabaninayak1990@gmail.com>
2019-07-19 08:00:14 -0500

    Updated java docs for services

--- src/main/java/com/walmart/matchingservices/service/MatchingService.java
+++ src/main/java/com/walmart/matchingservices/service/MatchingService.java
@@ -3,23 +3,31 @@ package com.walmart.matchingservices.service;
 import com.walmart.matchingservices.entity.Criteria;
 import com.walmart.matchingservices.entity.Technician;
 
+/**
+ * @author srabaninayak
+ *
+ */
+
 public interface MatchingService {
 	
 	/**
-	* The number of pending issues ready to be assigned *
-	* @return the number of pending issues */
+	* The number of pending issues ready to be assigned 
+	* @return the number of pending issues 
+	*/
 	int numIssues ();
+	
 	/**
-	* Find and assign a matching technician based on criteria and technician ability *
+	* Find and assign a matching technician based on criteria and technician ability
 	* @param Criteria needed to be matched
-	* @param customerEmail unique identifier for the customer * @return a Technician object with tech details
+	* @param customerEmail unique identifier for the customer 
+	* @return a Technician object with tech details
 	*/
 	Technician findAndAssignTecnician(Criteria criteria, String customerEmail);
+	
 	/**
 	* Assigned Issue
-	*
 	* @param issueId the issues identifier
-	* @param customerEmail the email address of the customer to which the seat hold is assigned
+	* @param customerEmail the email address of the customer to which the reservation is assigned
 	* @param techId the identifier of the technician
 	* @return a reservation confirmation code
 	*/
--- src/main/java/com/walmart/matchingservices/service/ProcessCustomerRequest.java
+++ src/main/java/com/walmart/matchingservices/service/ProcessCustomerRequest.java
@@ -2,10 +2,23 @@ package com.walmart.matchingservices.service;
 
 import com.walmart.matchingservices.dto.CustomerIssueRequest;
 
+/**
+ * @author srabaninayak
+ *
+ */
+
 public interface ProcessCustomerRequest {
-	
+		/**
+		* Process the customer request to reserve a technician and provide the reservation confirmation number to customer
+		* @param CustomerIssueRequest contains issue identifier and cutsomer details
+		* @return a reservation confirmation number
+		*/
 		String processRequest(CustomerIssueRequest customerIssueRequest);
 		
+		/**
+		* Returns number of currently pending issues from the system
+		* @return currently pending issues
+		*/
 		Integer numIssues();
 		
 }
